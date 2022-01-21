f70550b049b128dbe059b838a50cbe5ddf381f48
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-21 12:17:28 +0530

    Add project files.

---
+++ StudentWebAPI/Data/StudentWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentWebAPI.Model;
+
+namespace StudentWebAPI.Data
+{
+    public class StudentWebAPIContext : DbContext
+    {
+        public StudentWebAPIContext (DbContextOptions<StudentWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentWebAPI.Model.Student> Student { get; set; }
+    }
+}
